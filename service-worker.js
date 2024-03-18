9410d71029e1280ec0f4c044a1630691482b88e3
Felix T. Vogel <contact@housebird.games>
2024-03-18 15:16:32 +0100

    Updated Birdhouse

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-self.CACHE_VERSION = "2.0.0.60";
+self.CACHE_VERSION = "2.0.0.61";
 importScripts('./config-sw.js' + `?v=${self.CACHE_VERSION}`);
 self.importScripts('Birdhouse/filesToCache.js' + `?v=${self.CACHE_VERSION}`);
 
